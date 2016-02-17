9046e6ee3c2902c1abc7a3695da718317d1618c1
dijkmeijer <dijkmeijer@gmail.com>
2016-02-17 23:05:48 +0000

    installatie script

--- install/install.sh
+++ install/install.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
 echo "install wiringPi"
+getWiringPi.sh
 
 echo "install Asoundlib"
+getAsoundlib.sh 
 
 echo "installeer midiout als deamon"
+midioutOnStartup.sh
 
 echo "download en installeer vncserver als deamon"
+getTightVNCserver.sh 
+VNCOnStartup.sh
+
+echo "!! vncserver opstarten om wachtwoord te maken"
 
 echo "installeer inputs als deamon"
+inputOnStartup.sh
 
 echo "stel dhcpclient in op vast adres 192.168.1.10"
+setStaticAdress.sh
 
 echo "installeer seq24"
+getSeq24.sh
+
+echo "init git met username e.d."
+:wq
+initgit.sh
+
+
