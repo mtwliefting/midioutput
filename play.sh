bc81a1c37b81622294c13ecdfc7f21d938d31e24
Mander Liefting <52751000+mtwliefting@users.noreply.github.com>
2020-05-26 15:20:42 +0200

    Update play.sh

--- play.sh
+++ play.sh
@@ -4,7 +4,9 @@
 if [ "$(pidof aplay)" ]
 then
 	killall aplay
+	killall omxplayer
 else
         amixer cset numid=1 85%
-	nohup aplay "/home/pi/midioutput/Walking the Dog3.wav" </dev/null >/dev/null 2>&1 &    
+	nohup aplay "/home/pi/midioutput/Walking the Dog3.wav" </dev/null >/dev/null 2>&1 &   
+	nohup omxplayer -p -o hdmi /opt/vc/src/hello_pi/hello_video/test.h264 
 fi
