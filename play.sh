d773e8de6a436e0845c2e43f6c3b493911866e9a
Mander Liefting <52751000+mtwliefting@users.noreply.github.com>
2020-05-26 15:32:44 +0200

    Update play.sh

--- play.sh
+++ play.sh
@@ -8,5 +8,5 @@ then
 else
         amixer cset numid=1 85%
 	nohup aplay "/home/pi/midioutput/Walking the Dog3.wav" </dev/null >/dev/null 2>&1 &   
-	nohup omxplayer -p -o hdmi /opt/vc/src/hello_pi/hello_video/test.h264 
+	nohup omxplayer -p -o hdmi /opt/vc/src/hello_pi/hello_video/test.h264 </dev/null >/dev/null 2>&1 &
 fi
